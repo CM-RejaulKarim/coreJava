dc396581c9d95c96ab2a7a0340b37f9ca00f0b9c
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-03-24 14:49:26 +0600

    mar 24
    first code

---
+++ TestDataType/src/testdatatype/TestDataType.java
@@ -0,0 +1,25 @@
+package testdatatype;
+
+public class TestDataType {
+
+    public static void main(String[] args) {
+      
+      byte id =127;
+      short roll= 7759;
+      int number= 1287759;
+      long longNum = 1287759128775912877l;       
+//        long longNum = 1287759128775912877(l)=L ;
+//        ctrl + shift + down arrow -- for copy and paste bellow; 
+
+      float price = 128.258f;  
+      double dPrice = 128.258;  
+      
+      char letter= '\u0041';
+      String name = "rahim";
+      boolean status= true;
+      
+        System.out.println(name + letter +price);
+
+    }
+    
+}
