04c66e3bb7a2cc02153ac30a0ecb65ebcea951cb
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-05 18:54:55 +0600

    Eid Close
    interest calculation

---
+++ CalculateInterest/src/calculateinterest/CalculateInterest.java
@@ -0,0 +1,16 @@
+
+package calculateinterest;
+
+import java.util.Scanner;
+
+public class CalculateInterest {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner s= new Scanner(System.in);
+        
+        
+       
+    } 
+}
