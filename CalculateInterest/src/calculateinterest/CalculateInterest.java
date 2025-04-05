42c04d22d05c2c9eb9806e87e8aa66fed5ff053c
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-05 23:28:58 +0600

    interest calculation
    Calculate monthly/ Yearly interest .

--- CalculateInterest/src/calculateinterest/CalculateInterest.java
+++ CalculateInterest/src/calculateinterest/CalculateInterest.java
@@ -1,16 +1,29 @@
-
 package calculateinterest;
 
 import java.util.Scanner;
 
 public class CalculateInterest {
 
-   
     public static void main(String[] args) {
-        
-        Scanner s= new Scanner(System.in);
-        
-        
-       
-    } 
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter annual interest rate like as 7.25% ");
+        double annualInterest = s.nextDouble();
+
+        double monthlyInterestRate = annualInterest / 1200;
+
+        System.out.println("Enter Number of Year ");
+        int numberOfYear = s.nextInt();
+
+        System.out.println("Enter Invest Amount ");
+        double loanAmount = s.nextDouble();
+
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, numberOfYear * 12));
+
+        double totalPayment = monthlyPayment * numberOfYear * 12;
+
+        System.out.println("Monthly Payment $" + (int) monthlyPayment);
+        System.out.println("Total Payment $" + (int) totalPayment);
+    }
 }
