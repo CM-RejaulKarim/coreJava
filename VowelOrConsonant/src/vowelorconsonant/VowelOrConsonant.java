c6f09c67fa03499971a99094bdc98c92071583e7
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-07 12:06:14 +0600

    Vowe lOr Consonant

---
+++ VowelOrConsonant/src/vowelorconsonant/VowelOrConsonant.java
@@ -0,0 +1,30 @@
+package vowelorconsonant;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Latter ");
+        String uInp = sc.next();
+        String str = uInp.toLowerCase();
+        char latter = str.charAt(0);
+
+        if (latter == 'a' || latter == 'e' || latter == 'i' || latter == 'o' || latter == 'u') {
+            System.out.println(uInp + " is Vowel");
+        }
+        else if(latter>='a'&& latter<='z'){
+         System.out.println(uInp+" is Consonant");
+        }
+        else{
+         System.err.println(uInp+" is not a Latter");
+        }
+
+       
+
+    }
+
+}
