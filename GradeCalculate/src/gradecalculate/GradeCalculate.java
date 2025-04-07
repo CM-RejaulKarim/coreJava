ddeff52b1061b91799a2675c761458aa48139f29
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-07 12:20:11 +0600

    Grade Calculator using Marks

---
+++ GradeCalculate/src/gradecalculate/GradeCalculate.java
@@ -0,0 +1,39 @@
+package gradecalculate;
+
+import java.util.Scanner;
+
+
+public class GradeCalculate {
+
+    
+    public static void main(String[] args) {
+      
+        Scanner s  = new Scanner(System.in);
+        
+        System.out.println("Input Mark: ");
+        byte mark=s.nextByte();
+        
+        
+        if (mark<60) {
+            System.out.println(mark+" is Grade F");
+        }
+        else if(mark<70){
+        System.out.println(mark+" is Grade D");
+        }
+        else if(mark<80){
+        System.out.println(mark+" is Grade C");
+        }
+        else if(mark<90){
+        System.out.println(mark+" is Grade B");
+        }
+        else if(mark<=100){
+        System.out.println(mark+" is Grade A");
+        }
+        else{
+        System.err.println(mark+" is not Valid Mark");
+        }
+  
+        
+    }
+    
+}
