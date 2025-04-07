fb1d0784b55c484862d09abe99199d33782e7852
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-07 16:49:54 +0600

    Vowel and Consonant

---
+++ VowelConsonantUsingSwitch/src/vowelconsonantusingswitch/VowelConsonantUsingSwitch.java
@@ -0,0 +1,24 @@
+
+
+package vowelconsonantusingswitch;
+
+import java.util.Scanner;
+
+
+public class VowelConsonantUsingSwitch {
+
+ 
+    
+    public static void main(String[] args) {
+     
+        Scanner s= new Scanner(System.in);
+        
+        System.out.println("Enter a Latter ");
+        String inp= s.next();
+        
+        
+        
+        
+    }
+    
+}
--- VowelOrConsonant/src/vowelorconsonant/VowelOrConsonant.java
+++ VowelOrConsonant/src/vowelorconsonant/VowelOrConsonant.java
@@ -7,21 +7,25 @@ public class VowelOrConsonant {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a Latter ");
-        String uInp = sc.next();
-        String str = uInp.toLowerCase();
-        char latter = str.charAt(0);
-
-        if (latter == 'a' || latter == 'e' || latter == 'i' || latter == 'o' || latter == 'u') {
-            System.out.println(uInp + " is Vowel");
-        }
-        else if(latter>='a'&& latter<='z'){
-         System.out.println(uInp+" is Consonant");
-        }
-        else{
-         System.err.println(uInp+" is not a Latter");
-        }
+        
+        
+        
+        
+//      1st Attempt
+//        System.out.println("Enter a Latter ");
+//        String uInp = sc.next();
+//        String str = uInp.toLowerCase();
+//        char latter = str.charAt(0);
+//
+//        if (latter == 'a' || latter == 'e' || latter == 'i' || latter == 'o' || latter == 'u') {
+//            System.out.println(uInp + " is Vowel");
+//        }
+//        else if(latter>='a'&& latter<='z'){
+//         System.out.println(uInp+" is Consonant");
+//        }
+//        else{
+//         System.err.println(uInp+" is not a Latter");
+//        }
 
        
 
