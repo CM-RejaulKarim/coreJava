76bca7a5ddfe7bb72e0531ee2947e159ca493f6c
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-05-07 18:44:24 +0600

    Class 070may

--- TestFileStream/src/testfilestream/TestFileStream.java
+++ TestFileStream/src/testfilestream/TestFileStream.java
@@ -3,12 +3,10 @@ package testfilestream;
 public class TestFileStream {
 
     public static void main(String[] args) {
+        
+        int[] list = {2, 9, 5, 4, 8, 1, 6};
 
-        
-        
-        
-        
-        
+              
     }
 
 }
